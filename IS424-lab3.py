60ab01f808777d8efcc2f05dfd1c20ae519cbab7
yasser abdullah <145041275+yasserAbdullah-1@users.noreply.github.com>
2024-02-15 18:48:53 +0300

    Update IS424-lab3.py

--- IS424-lab3.py
+++ IS424-lab3.py
@@ -1,34 +1,34 @@
 #1
-# dicto={}
-# name=input("enter the name")
-# while name!='no':
-#     salary = input("enter the salary")
-#     dicto[name]=salary
-#     name = input("enter the name")
-# print(dicto)
+dicto={}
+name=input("enter the name")
+while name!='no':
+    salary = input("enter the salary")
+    dicto[name]=salary
+    name = input("enter the name")
+print(dicto)
 
 #2
-# numbers=[]
-#
-# for i in range(10):
-#     newNum=int(input("enter the number"))
-#     numbers.append(newNum)
-#     if len(numbers)==1:
-#         maxo=numbers[0]
-#     else:
-#         if maxo<newNum:
-#             maxo=newNum
-# print(maxo)
-# # print(numbers)
-# #3
-# cel=float(input('enter the temperature'))
-# print((9/5)*cel+32)
-# #4
-# numo=int(input("enter number"))
-# for i in range(1,11):
-#     print(f'{i}*{numo} = {i*numo}')
-#
-# #5
+numbers=[]
+
+for i in range(10):
+    newNum=int(input("enter the number"))
+    numbers.append(newNum)
+    if len(numbers)==1:
+        maxo=numbers[0]
+    else:
+        if maxo<newNum:
+            maxo=newNum
+print(maxo)
+print(numbers)
+#3
+cel=float(input('enter the temperature'))
+print((9/5)*cel+32)
+#4
+numo=int(input("enter number"))
+for i in range(1,11):
+    print(f'{i}*{numo} = {i*numo}')
+
+#5
 x= int(input("Enter a number of repetitions"))
 # Write your decorator here
 def annouce(f):
